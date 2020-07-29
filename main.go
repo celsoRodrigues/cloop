94eb48c574e37f16b721cd4b375f4255335b08b6
celso rodrigues <celso.rodrigues@hotmail.co.uk>
2020-07-29 17:56:10 +0100

    adding new folder to the project

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"bufio"
+	"encoding/xml"
 	"fmt"
 	"html/template"
-	"log"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
 
+	"github.com/celsoRodrigues/cloop/model"
 	"github.com/nguyenthenguyen/docx"
 )
 
@@ -133,116 +133,28 @@ var fn = template.FuncMap{
 
 func main() {
 
-	r, err := docx.ReadDocxFile(filepath.Join("./docx", "Week_28.docx"))
+	r, err := docx.ReadDocxFile(filepath.Join("./docx", "hp2.docx"))
 	defer r.Close()
 
 	if err != nil {
 		panic(err)
 	}
 
-	mycapaign := campaign{}
-	link := linkStruct{}
+	//fmt.Print(r.Content)
+	myDoc := model.Document{}
 
-	var cr = regexp.MustCompile(`</w:p>`)
-	var re = regexp.MustCompile(`[<][^>]*[>]`)
-
-	str := cr.ReplaceAll([]byte(r.Content), []byte("\n"))
-	str = re.ReplaceAll([]byte(str), []byte(""))
-	fmt.Print(string(str))
-	scanner := bufio.NewScanner(strings.NewReader(string(str)))
-
-	var week string
-	var links []string
-	var linksCopyLines []string
-	var titleLines []string
-	var bodyLines []string
-	var ctaLines []string
-
-	for scanner.Scan() {
-
-		if strings.Contains(scanner.Text(), "Week:") {
-			index := strings.Index(scanner.Text(), ":")
-			week = strings.Trim(scanner.Text()[index+1:], " ")
-		}
-
-		if strings.Contains(scanner.Text(), "Top Link:") {
-			index := strings.Index(scanner.Text(), ":")
-			link := strings.Trim(scanner.Text()[index+31:], " ")
-			links = append(links, link)
-		}
-
-		if strings.Contains(scanner.Text(), "Top Copy") {
-			scanner.Scan()
-			linksCopyLines = append(linksCopyLines, scanner.Text())
-		}
-
-		if strings.Contains(scanner.Text(), "Title copy") {
-			scanner.Scan()
-			titleLines = append(titleLines, scanner.Text())
-		}
-
-		if strings.Contains(scanner.Text(), "Body copy") {
-			scanner.Scan()
-			bodyLines = append(bodyLines, scanner.Text())
-		}
-
-		if strings.Contains(scanner.Text(), "CTA copy") {
-			var s string
-			for {
-				scanner.Scan()
-				if len(scanner.Text()) == 0 {
-					break
-				}
-				s += scanner.Text() + ", "
-			}
-			ctaLines = append(ctaLines, strings.Trim(s, ", "))
-		}
-	}
-
-	if len(titleLines) != len(bodyLines) || len(ctaLines) != len(titleLines) || len(bodyLines) != len(ctaLines) {
-		fmt.Println("There is a field(s) missing in the page\n You have:\n titles:", len(titleLines), "Bodies:", len(bodyLines), "Cta's:", len(ctaLines))
-		os.Exit(1)
-	}
-
-	if len(links) != len(linksCopyLines) {
-		fmt.Println("There is a field(s) missing in the page\n You have:\n links:", len(links), "\n linkCopy:", len(linksCopyLines))
-		os.Exit(1)
-	}
-
-	var mycampaigns []campaign
-	var myLinks []linkStruct
-	for i := 0; i < len(titleLines); i++ {
-
-		mycapaign.Title = titleLines[i]
-		mycapaign.Body = bodyLines[i]
-		mycapaign.Cta = ctaLines[i]
-
-		mycampaigns = append(mycampaigns, mycapaign)
-
-	}
-
-	for i := 0; i < len(links); i++ {
-
-		link.URL = links[i]
-		link.Copy = linksCopyLines[i]
-
-		myLinks = append(myLinks, link)
-
-	}
-
-	mkt := marketing{
-		Week:      week,
-		Campaigns: mycampaigns,
-		Links:     myLinks,
-	}
-
-	fd, err := os.OpenFile(filepath.Join("./bin", "page.html"), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
-	defer fd.Close()
-
-	tmpl := template.Must(template.New("layout.html").Funcs(fn).ParseFiles(filepath.Join("./layout", "layout.html")))
-	err = tmpl.Execute(fd, mkt)
+	err = xml.Unmarshal([]byte(r.Content), &myDoc)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	for i := 0; i <= len(myDoc.Body.P)-1; i++ {
+
+		if myDoc.Body.P[i].Sdt.SdtPr.Tag.Val == "week" {
+			fmt.Printf("%s: ", myDoc.Body.P[i].Sdt.SdtPr.Tag.Val)
+			fmt.Printf("%v\n", myDoc.Body.P[i].Sdt.SdtContent.R[0].T.Text)
+		}
 	}
 
 }
